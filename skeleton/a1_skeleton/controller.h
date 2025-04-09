d2320a5009d1aaa5230c8ea416ae03f95c5c3e9b
AmerAbuIssa <81179476+AmerAbuIssa@users.noreply.github.com>
2025-04-10 04:45:12 +1000

    Update controller.h

--- skeleton/a1_skeleton/controller.h
+++ skeleton/a1_skeleton/controller.h
@@ -11,7 +11,34 @@ public:
   Controller();
   ~Controller();
 
-  //See controllerinterface.h for more information
+  // Common functionality for all controllers
+  bool reachGoal(void) override;
+  bool setGoal(pfms::geometry_msgs::Point goal) override;
+  double distanceToGoal(void) override;
+  double timeToGoal(void) override;
+  bool setTolerance(double tolerance) override;
+  double distanceTravelled(void) override;
+  double timeInMotion(void) override;
+  pfms::nav_msgs::Odometry getOdometry(void) override;
+  
+  // Pure virtual functions to be implemented by derived classes
+  virtual bool checkOriginToDestination(pfms::nav_msgs::Odometry origin,
+                                        pfms::geometry_msgs::Point goal,
+                                        double& distance,
+                                        double& time,
+                                        pfms::nav_msgs::Odometry& estimatedGoalPose) override = 0;
+  
+  virtual pfms::PlatformType getPlatformType(void) override = 0;
+
+protected:
+  // Common attributes for all controllers
+  PfmsConnector* pfmsConnectorPtr_; // Only one instance per controller
+  pfms::geometry_msgs::Point goal_;
+  double tolerance_;
+  double distanceToGoal_;
+  double timeToGoal_;
+  bool goalSet_;
+  pfms::nav_msgs::Odometry estimatedGoalPose_;
 };
 
 #endif // CONTROLLER_H
